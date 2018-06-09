03e00df05e9a00b7d9fc68d97e660129c14e1f4d
Bgoldfa <bgoldfa@gap.com>
2018-06-09 19:01:25 -0400

    Commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-var path = require('path');
-var express = require('express');
+const path = require('path');
+const express = require('express');
+const port = process.env.PORT || 8080
+const app = express();
 
-var app = express();
+app.use(express.static(__dirname));
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 8080);
-
-var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
 });
+
+app.listen(port)
+console.log("Server Started");
